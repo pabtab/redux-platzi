6f46795725030d8b7c1054c89e16f1b56682a316
Pablo Tabares Hernandez <pablother3@gmail.com>
2018-10-31 13:58:06 -0500

    init project and made the form

---
+++ src/entries/redux.js
@@ -0,0 +1,12 @@
+console.log('oli')
+
+const $form = document.getElementById('form');
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event){
+    event.preventDefault();
+    const data = new FormData($form)
+    const title = data.get('title')
+
+    console.log(title)
+}
