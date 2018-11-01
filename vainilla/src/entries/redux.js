2be3b14c51590a608331b0061b9b3ddd9fc9aaf0
Pablo Tabares Hernandez <pablother3@gmail.com>
2018-10-31 23:15:26 -0500

    redux cycle done

--- vainilla/src/entries/redux.js
+++ vainilla/src/entries/redux.js
@@ -3,44 +3,64 @@ import { createStore } from 'redux';
 const $form = document.getElementById('form');
 $form.addEventListener('submit', handleSubmit)
 
-function handleSubmit(event){
-    event.preventDefault();
-    const data = new FormData($form)
-    const title = data.get('title')
-    console.log(title)
+function handleSubmit(event) {
+	event.preventDefault();
+	const data = new FormData($form)
+	const title = data.get('title')
+	console.log(title)
 
-    store.dispatch({
-        type: 'ADD_SONG',
-        payload: {
-            title
-        }
-    })
+	store.dispatch({
+		type: 'ADD_SONG',
+		payload: {
+			title
+		}
+	})
 }
 
 const initialState = [
-    {
-        title: 'Despacito'
-    },
-    {
-        title: 'One more time'
-    },
-    {
-        title: 'Echame la culpa'
-    }
-]
+	{
+		title: 'Despacito'
+	},
+	{
+		title: 'One more time'
+	},
+	{
+		title: 'Echame la culpa'
+	}
+];
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD_SONG':
+			return [...state, action.payload]
+			
+		default:
+			return state;
+	}
+}
 
 const store = createStore(
-    (state) => state,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	reducer,
+	initialState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-const $container = document.getElementById('playlist');
-const playlist = store.getState();
-playlist.forEach(item => {
-    const template = document.createElement('p');
-    template.textContent = item.title;
-    $container.appendChild(template);
-})
 
-console.log(store.getState())
+function render() {
+	const $container = document.getElementById('playlist');
+	const playlist = store.getState();
+	$container.innerHTML = '';
+	playlist.forEach(item => {
+		const template = document.createElement('p');
+		template.textContent = item.title;
+		$container.appendChild(template);
+	})
+}
+
+function handleChange() {
+	render()
+}
+
+render();
+
+store.subscribe(handleChange);
